2ee1bdef9173dcabda47476ffffdb6dbc1b6abc4
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2021-06-02 11:07:24 +0700

    fix(auth): missing role on validate access token

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -16,6 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    return { id: payload.sub, email: payload.email };
+    return { id: payload.sub, email: payload.email, role: payload.role };
   }
 }
